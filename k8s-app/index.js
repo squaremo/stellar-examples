d3a747bdf281f81f93ea044ec922c520202698b9
Michael Bridgen <mbridgen@pulumi.com>
2022-05-30 10:17:22 +0100

    Simplest example of building/running in Kubernetes
    The general idea of Stellar programs is to go from a directory (or
    directories) of app code to running containers. This program is a first
    attempt: build the current directory using Docker, then create a
    deployment in whichever Kubernetes you're pointed at.
    
    Signed-off-by: Michael Bridgen <mbridgen@pulumi.com>

---
+++ k8s-app/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const docker = require("@pulumi/docker");
+const k8s = require("@pulumi/kubernetes");
+
+const image = new docker.Image("app-image", {
+    build: ".",
+    imageName: "app",
+});
+
+const appLabels = { app: "app" };
+const deployment = new k8s.apps.v1.Deployment("app", {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: app, image: image.imageName }] }
+        }
+    }
+});
+
+exports.image = image.imageName;
+exports.name = deployment.metadata.name;
